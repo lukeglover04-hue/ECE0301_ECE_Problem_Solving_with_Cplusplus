14829d3f4d785319ca4cabcf82694df25309203e
lukeglover04-hue <lukeglover04@gmail.com>
2026-09-14 14:11:08 +0000

    finish radius input validation

--- Lectures/1B_cli_file_io/cli_file_io_starter/cli_file_io_starter.cpp
+++ Lectures/1B_cli_file_io/cli_file_io_starter/cli_file_io_starter.cpp
@@ -3,29 +3,38 @@
 #include <iomanip>
 #include <iostream>
 
-int main()
+int main(int argc, char *argv[])
 {
+
+  std::cout << "What is argc: " << argc << std::endl;
+  std::cout << "What is argv(0)" << argv[0] << std::endl;
+  //std::cout << "What is argv(1)" << argv[1] << std::endl;
+  //std::cout << "What is argv(2)" << argv[2] << std::endl;
+
   char answer = 'y';
 
   while (answer == 'y' || answer == 'Y')
   {
-    double radius;
+    double radius = 4.0;
     std::cout << "Enter the radius of the circle: ";
     std::cin >> radius;
+    bool isNumber = std::cin.good();
+    bool isPositive = (radius >= 0);
 
-    if (!std::cin.good())
+    if (!isNumber)
     {
       std::cout << "Invalid input type" << std::endl;
       return EXIT_FAILURE;
     }
 
-    if (radius < 0)
+    if (!isPositive)
     {
       std::cout << "Invalid radius, must be non-negative" << std::endl;
       return EXIT_FAILURE;
     }
 
     double area = M_PI * std::pow(radius, 2);
+
     std::cout << "The area is: ";
     std::cout << std::fixed << std::setprecision(5);
     std::cout << area << std::endl;
